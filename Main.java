740062e8baab6f628a032d828e84bd7ed03dd11e
jjohnddl <johndalegalera0@gmail.com>
2026-03-30 12:34:56 +0800

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,40 +1,60 @@
-package arrays;
 import java.util.Scanner;
-
-public class Main {
-    public static int[][] arrays = new int[3][3];
-static void getinfo(Scanner sc){
-    System.out.println("===Input Student Grades===");
-    for(int i =0;i< arrays.length;i++){
-        System.out.println("Student #"+(i+1));
-        for(int j=0;j<arrays[i].length;j++){
-        System.out.print("Grade #"+(j+1)+": ");
-        arrays[i][j] = sc.nextInt();
+public class Main{
+    static String[][] seats = new String[5][5];
+public static void initializeSeats(){
+    for(int i = 0; i<seats.length; i++){
+        for(int j = 0; j<seats[i].length;j++){
+seats[i][j] = "O";
         }
-        System.out.println();
     }
 }
-static void displaygrades(){
-    System.out.println();
-    System.out.println("===Table Grades===");
-    System.out.print("        ");
-    for(int j = 0; j<arrays[0].length;j++){
-        System.out.printf("Stdt #%-2d",(j+1));
-    }
-    System.out.println();
-    for(int i =0;i<arrays.length;i++){
-        System.out.printf("Grade #%-3d",(i+1));
-        for(int j = 0;j<arrays[i].length;j++){
-            System.out.printf("%-5s  ",arrays[i][j]);
+   public static void displayseats(){
+ System.out.println("Cinema seats: ");
+ System.out.println("  1 2 3 4 5 ");
+ for(int i = 0; i<seats.length;i++){
+    System.out.print((i+1) + " ");
+    for(int j =0;j<seats[i].length;j++){
+    System.out.print(seats[i][j]+" ");
+ }
+  System.out.println();
+}
+}
+   public static void reserveseats(int row,int col){
+        if(row < 0 || row >= 5 || col < 0 || col >= 5){
+            System.out.println("Invalid Seat");
+            return;
         }
-        System.out.println();
+        if(seats[row][col].equals("O")){
+            seats[row][col] = "X";
+            System.out.println("Seat Reserve");
+        }
+        else{
+            System.out.println("Seat Taken");
+        }
     }
-}
 
-    public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-  getinfo(sc);
-  displaygrades();
-}
+public static void main(String[] args) {
+Scanner sc =  new Scanner(System.in);
+initializeSeats();
+    
+while (true) { 
+    displayseats();
 
+    System.out.println("Enter row (1-5) or 0 to exit");
+    int row = sc.nextInt();
+
+    if (row == 0){
+        System.out.println("Exiting...");
+        return;
+    }
+     row = row - 1;
+
+    System.out.println("Enter Colum: ");
+    int col = sc.nextInt();
+    col = col - 1;
+    reserveseats(row,col);
+
+
+}
+}
 }
